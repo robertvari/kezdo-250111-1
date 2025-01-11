d7f02e8b9840ce9a221ff7382f9121384a732001
Robert Vari <mail.robertvari@gmail.com>
2025-01-11 15:54:53 +0100

    update

---
+++ 03_variables.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+address = input("Where do you live?")
+phone = input("What is your phone number?")
+
+# f = formated string
+print(  f"Hello {name}. You live in {address}. Your phone number is: {phone}"  )
