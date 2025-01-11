4c2c37288fd4c6d71c48459fa4d3b39a14ea85e1
Robert Vari <mail.robertvari@gmail.com>
2025-01-11 17:39:01 +0100

    update

---
+++ 11_slicing.py
@@ -0,0 +1,8 @@
+lucky_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# your_list[START:STOP:STEP]
+print(lucky_numbers[0:4])
+print(lucky_numbers[:4])
+print(lucky_numbers[4:])
+print(lucky_numbers[::2])
+print(lucky_numbers[::-1])
