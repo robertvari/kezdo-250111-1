2dbbc0457d7c344542a43abdebda33c01189a6f0
Robert Vari <mail.robertvari@gmail.com>
2025-01-12 17:28:44 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -3,7 +3,6 @@ import os, platform
 min_number = 1
 max_number = 10
 max_tries = 3
-magic_number = 5
 
 # Clear terminal for the game
 if platform.system() == "Windows":
@@ -18,6 +17,16 @@ print("-"*50, "MAGIC NUMBER", "-"*50)
 print(f"I have a number between {min_number} and {max_number}. Can you guess it?")
 print(f"You can try {max_tries} times.")
 
-# TODO ask player for their number
+magic_number = str(5)
+
+# ask player for their number
+player_number = input("What is my number? ")
+
 # TODO do a check if their number == magic_number
+while magic_number != player_number:
+    print("Wrong guess. Try again.")
+    player_number = input("What is my number? ")
+
+
 # TODO endgame condition
+print("You guessed my number! Congrats!!!!")
