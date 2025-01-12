ebf2d6bc7fd675dae7cdf43be97ceebe94cb173f
Robert Vari <mail.robertvari@gmail.com>
2025-01-12 16:38:56 +0100

    update

---
+++ 23_for_loop.py
@@ -0,0 +1,4 @@
+my_friends = ["Csaba", "Kriszta", "Tamás", "Ádám"]
+
+for i in my_friends:
+    print(i.upper())
---
+++ 24_for_continue.py
@@ -0,0 +1,6 @@
+my_friends = ["Csaba", "Kriszta", "Tamás", "Ádám"]
+
+for i in my_friends:
+    if i == "Kriszta":
+        continue  # skip item
+    print(i.upper())
