079d45f19ff009c01811fc159921744d9db4ff73
Robert Vari <mail.robertvari@gmail.com>
2025-01-12 17:56:13 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -3,24 +3,24 @@ import os, platform, random
 min_number = 1
 max_number = 10
 
-# Clear terminal for the game
-if platform.system() == "Windows":
-    os.system("cls")
-elif platform.system() == "Darwin":
-    os.system("clear")
-elif platform.system() == "Linux":
-    os.system("clear")
-
-# print an introduction of the game
-print("-"*50, "MAGIC NUMBER", "-"*50)
-
 while True:  # start game loop
+    # Clear terminal for the game
+    if platform.system() == "Windows":
+        os.system("cls")
+    elif platform.system() == "Darwin":
+        os.system("clear")
+    elif platform.system() == "Linux":
+        os.system("clear")
+
     max_tries = 3
+
+    # print an introduction of the game
+    print("-"*50, "MAGIC NUMBER", "-"*50)
     print(f"I have a number between {min_number} and {max_number}. Can you guess it?")
     print(f"You can try {max_tries} times.")
 
     magic_number = str( random.randint(min_number, max_number) )
-    print(f"HINT: {magic_number}")
+    # print(f"HINT: {magic_number}")
 
     # ask player for their number
     player_number = input("What is my number? ")
